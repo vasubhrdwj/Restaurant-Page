54c31702fcb004f3baa514dbf9485fbfea4305a0
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-02-26 18:06:03 +0530

    Added the mainText using homepage module

--- src/home-page.js
+++ src/home-page.js
@@ -1,3 +1,11 @@
 export default function addContent() {
-  
+  const contentDiv = document.querySelector("#content");
+
+  const tagLine = document.createElement("div");
+  const mainText = document.createElement("h1");
+  tagLine.classList.add("tagLine");
+  mainText.textContent = `More Than Coffee,
+          It's an Experience.`;
+  tagLine.appendChild(mainText);
+  contentDiv.appendChild(tagLine);
 }
